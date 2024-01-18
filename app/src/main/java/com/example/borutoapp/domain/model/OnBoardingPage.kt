e65530dff2d9f085363f763634443223ca6a333e
Ali <ali.cast.santos@gmail.com>
2024-01-18 11:30:00 -0300

    Start welcome screen implementation

---
+++ app/src/main/java/com/example/borutoapp/domain/model/OnBoardingPage.kt
@@ -0,0 +1,29 @@
+package com.example.borutoapp.domain.model
+
+import androidx.annotation.DrawableRes
+import com.example.borutoapp.R
+
+sealed class OnBoardingPage(
+    @DrawableRes
+    val image: Int,
+    val title: String,
+    val description: String
+) {
+    object First : OnBoardingPage(
+        image = R.drawable.greetings,
+        title = "Greetings",
+        description = "App that displays some Boruto characters!"
+    )
+
+    object Second : OnBoardingPage(
+        image = R.drawable.explore,
+        title = "Explore",
+        description = "Find your favorite heroes and learn some of the things that you didn't know about."
+    )
+
+    object Third : OnBoardingPage(
+        image = R.drawable.power,
+        title = "Power",
+        description = "Check out your hero's power and  see how much are they strong comparing to others."
+    )
+}
