d05da43e5fadc3e3e4ad1125fdd14961dbb441c1
Ali <ali.cast.santos@gmail.com>
2024-01-18 18:57:47 -0300

    Add rounded corners to search bar

--- app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeTopBar.kt
+++ app/src/main/java/com/example/borutoapp/presentation/screens/home/HomeTopBar.kt
@@ -1,11 +1,20 @@
 package com.example.borutoapp.presentation.screens.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.borutoapp.R
 import com.example.borutoapp.ui.theme.topAppBarBackgroundColor
 import com.example.borutoapp.ui.theme.topAppBarContentColor
@@ -19,11 +28,18 @@ fun HomeTopBar(onSearchClicked: () -> Unit) {
                 color = MaterialTheme.colors.topAppBarContentColor
             )
         },
-        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor,
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp,
+        modifier = Modifier.background(
+            color = MaterialTheme.colors.topAppBarBackgroundColor,
+            shape = RoundedCornerShape(0.dp, 0.dp, 32.dp, 32.dp),
+        ).padding(horizontal = 4.dp),
+
         actions = {
             IconButton(onClick = onSearchClicked) {
                 Icon(
                     imageVector = Icons.Default.Search,
+                    tint = MaterialTheme.colors.topAppBarContentColor,
                     contentDescription = stringResource(R.string.search_icon)
                 )
             }
