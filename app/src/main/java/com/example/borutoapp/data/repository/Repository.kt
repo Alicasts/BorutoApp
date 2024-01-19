9063e9656026b0c6c210627e8a952414b110d7c4
Ali <ali.cast.santos@gmail.com>
2024-01-19 14:14:33 -0300

    Create RemoteDataSource and starts its impl

--- app/src/main/java/com/example/borutoapp/data/repository/Repository.kt
+++ app/src/main/java/com/example/borutoapp/data/repository/Repository.kt
@@ -1,12 +1,24 @@
 package com.example.borutoapp.data.repository
 
+import androidx.paging.PagingData
+import com.example.borutoapp.domain.model.Hero
 import com.example.borutoapp.domain.repository.DataStoreOperations
+import com.example.borutoapp.domain.repository.RemoteDataSource
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class Repository @Inject constructor(
+    private val remote: RemoteDataSource,
     private val dataStore: DataStoreOperations
 ) {
+
+    fun getAllHeroes(): Flow<PagingData<Hero>> {
+        return remote.getAllHeroes()
+    }
+
+    fun searchHeroes(query: String): Flow<PagingData<Hero>> {
+        return remote.searchHeroes(query = query)
+    }
 
     suspend fun saveOnBoardingState(completed: Boolean) {
         dataStore.saveOnBoardingState(completed = completed)
@@ -15,4 +27,5 @@ class Repository @Inject constructor(
     fun readOnBoardingState(): Flow<Boolean> {
         return dataStore.readOnBoardingState()
     }
+
 }
