fa561a9eaa577c553ceeab59d9323f78c3c336fa
Ali <ali.cast.santos@gmail.com>
2024-01-25 11:08:54 -0300

    Update splash approach

--- app/src/main/java/com/example/borutoapp/MainActivity.kt
+++ app/src/main/java/com/example/borutoapp/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import coil.annotation.ExperimentalCoilApi
+import com.example.borutoapp.domain.use_cases.UseCases
+import com.example.borutoapp.navigation.Screen
 import com.example.borutoapp.navigation.SetupNavGraph
 import com.example.borutoapp.ui.theme.BorutoAppTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @ExperimentalCoilApi
 @ExperimentalAnimationApi
@@ -20,12 +28,28 @@ class MainActivity : ComponentActivity() {
 
     private lateinit var navController: NavHostController
 
+    @Inject
+    lateinit var useCases: UseCases
+
+    private var completed = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             BorutoAppTheme {
                 navController = rememberNavController()
-                SetupNavGraph(navController = navController)
+                val startDestination = if(completed) Screen.Home.route else Screen.Welcome.route
+                SetupNavGraph(
+                    navController = navController,
+                    startDestination = startDestination
+                )
+            }
+        }
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            useCases.readOnBoardingUseCase().collect {
+                completed = it
             }
         }
     }
